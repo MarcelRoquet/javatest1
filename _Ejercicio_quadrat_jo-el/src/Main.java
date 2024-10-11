06b68ff481971ba581efb12bd8c32bd69ebde1eb
JavierGrima <javier.grima@gracia.lasalle.cat>
2024-10-11 10:31:46 +0200

    Add files via upload

---
+++ _Ejercicio_quadrat_jo-el/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        int numero;
+        System.out.print("Cuanta es la longitud del lado del cuadrado?: ");
+        Scanner Scanner = new Scanner.nextDobule();
+        double area = numero * numero;
+        System.out.print("El area del cuadrado es"+area);
+        }
+    }
